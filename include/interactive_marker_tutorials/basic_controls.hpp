5736573bb64dac96b59159207e8625025a772a37
RaviRoKes <keshari.ravi2086@gmail.com>
2024-12-10 13:07:11 +0100

    3rd update with no compile error but couldn't see pannel

--- include/interactive_marker_tutorials/basic_controls.hpp
+++ include/interactive_marker_tutorials/basic_controls.hpp
@@ -1,47 +1,83 @@
 #ifndef BASIC_CONTROLS_HPP
 #define BASIC_CONTROLS_HPP
 
-#include <chrono>
-#include <memory>
-#include <string>
-#include <rclcpp/rclcpp.hpp>
-#include <interactive_markers/interactive_marker_server.hpp>
-#include <interactive_markers/menu_handler.hpp>
-#include <tf2/LinearMath/Transform.h>
+#include "interactive_markers/interactive_marker_server.hpp"
+#include "interactive_markers/menu_handler.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "tf2/LinearMath/Transform.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Vector3.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 #include <tf2_ros/transform_broadcaster.h>
-#include <visualization_msgs/msg/interactive_marker.hpp>
-#include <visualization_msgs/msg/interactive_marker_control.hpp>
-#include <visualization_msgs/msg/interactive_marker_feedback.hpp>
-#include <visualization_msgs/msg/marker.hpp>
+
+#include "visualization_msgs/msg/interactive_marker.hpp"
+#include "visualization_msgs/msg/interactive_marker_control.hpp"
+#include "visualization_msgs/msg/interactive_marker_feedback.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+
+#include "rviz_common/panel.hpp" // Include for RViz2 panel
+#include <QPushButton>           // Include for UI controls in the panel
+#include <QVBoxLayout>           // Include for layout management
 
 namespace interactive_marker_tutorials
 {
 
-    class BasicControlsNode : public rclcpp::Node
+  class BasicControlsNode : public rclcpp::Node
+  {
+  public:
+    explicit BasicControlsNode(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
+
+    ~BasicControlsNode() = default;
+
+    inline void applyChanges()
     {
+      server_->applyChanges();
+    }
+    // Helper Methods for Marker Control
+    visualization_msgs::msg::Marker makeBox(const visualization_msgs::msg::InteractiveMarker &msg);
+    visualization_msgs::msg::InteractiveMarkerControl &makeBoxControl(visualization_msgs::msg::InteractiveMarker &msg);
+
+    // Marker creation functions
+    void make6DofMarker(bool fixed, unsigned int interaction_mode, const tf2::Vector3 &position, bool show_6dof);
+    void makeRandomDofMarker(const tf2::Vector3 &position);
+    void makeViewFacingMarker(const tf2::Vector3 &position);
+    void makeQuadrocopterMarker(const tf2::Vector3 &position);
+    void makeChessPieceMarker(const tf2::Vector3 &position);
+    void makePanTiltMarker(const tf2::Vector3 &position);
+    void makeMenuMarker(const tf2::Vector3 &position);
+    void makeButtonMarker(const tf2::Vector3 &position);
+    void makeMovingMarker(const tf2::Vector3 &position);
+
+    // RViz Panel for Basic Controls
+    class BasicControlsPanel : public rviz_common::Panel
+    {
+      Q_OBJECT
     public:
-        explicit BasicControlsNode(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
-        ~BasicControlsNode() = default;
+      BasicControlsPanel(QWidget *parent = nullptr);
+      ~BasicControlsPanel();
+
+    protected:
+      void initializePanel();
 
     private:
-        void frameCallback();
-        void processFeedback(const visualization_msgs::msg::InteractiveMarkerFeedback::ConstSharedPtr &feedback);
-        void alignMarker(const visualization_msgs::msg::InteractiveMarkerFeedback::ConstSharedPtr &feedback);
-        visualization_msgs::msg::Marker makeBox(const visualization_msgs::msg::InteractiveMarker &msg);
-        visualization_msgs::msg::InteractiveMarkerControl &makeBoxControl(visualization_msgs::msg::InteractiveMarker &msg);
-        void make6DofMarker(bool fixed, unsigned int interaction_mode, const tf2::Vector3 &position, bool show_6dof);
-        void makeRandomDofMarker(const tf2::Vector3 &position);
-        void makeViewFacingMarker(const tf2::Vector3 &position);
-        void makeQuadrocopterMarker(const tf2::Vector3 &position);
-        void makeChessPieceMarker(const tf2::Vector3 &position);
-        void makePanTiltMarker(const tf2::Vector3 &position);
-
-        std::unique_ptr<interactive_markers::InteractiveMarkerServer> server_;
-        interactive_markers::MenuHandler menu_handler_;
-        std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-        rclcpp::TimerBase::SharedPtr frame_timer_;
+      QPushButton *button_; // Button to interact with the markers
     };
 
-} // namespace interactive_marker_tutorials
+  private:
+    void frameCallback(); // This method periodically broadcasts transform data for moving and rotating frames
+    // This method processes the feedback from user interactions with the interactive marker (such as button clicks or pose updates).
+    void processFeedback(const visualization_msgs::msg::InteractiveMarkerFeedback::ConstSharedPtr &feedback);
+    // This method aligns the marker to a grid when the user updates its pose.
+    void alignMarker(const visualization_msgs::msg::InteractiveMarkerFeedback::ConstSharedPtr &feedback);
 
+    std::unique_ptr<interactive_markers::InteractiveMarkerServer> server_; // A unique pointer to an InteractiveMarkerServer, which manages the interactive markers.
+    // An instance of MenuHandler for handling the context menu interactions with markers.
+    interactive_markers::MenuHandler menu_handler_;
+    // A unique pointer to a TransformBroadcaster, used for sending transform updates.
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+    // A shared pointer to a timer used for periodic updates, likely to call frameCallback.
+    rclcpp::TimerBase::SharedPtr frame_timer_;
+  };
+
+} // namespace interactive_marker_tutorials
 #endif // BASIC_CONTROLS_HPP
--- src/basic_controls.cpp
+++ src/basic_controls.cpp
@@ -1,113 +1,169 @@
 #include "interactive_marker_tutorials/basic_controls.hpp"
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-
-
+#include <chrono>
 #include <sstream>
 
 namespace interactive_marker_tutorials
 {
 
-    BasicControlsNode::BasicControlsNode(const rclcpp::NodeOptions &options)
-        : rclcpp::Node("basic_controls", options), menu_handler_()
-    {
-        // creates a unique pointer for an InteractiveMarkerServer class
-        server_ = std::make_unique<interactive_markers::InteractiveMarkerServer>(
-            "basic_controls",
-            get_node_base_interface(),
-            get_node_clock_interface(),
-            get_node_logging_interface(),
-            get_node_topics_interface(),
-            get_node_services_interface());
+BasicControlsNode::BasicControlsNode(const rclcpp::NodeOptions & options)
+: rclcpp::Node("basic_controls", options),
+  menu_handler_()
+{
+  server_ = std::make_unique<interactive_markers::InteractiveMarkerServer>(
+    "basic_controls",
+    get_node_base_interface(),
+    get_node_clock_interface(),
+    get_node_logging_interface(),
+    get_node_topics_interface(),
+    get_node_services_interface());
 
-        menu_handler_.insert("First Entry", std::bind(&BasicControlsNode::processFeedback, this, std::placeholders::_1));
-        menu_handler_.insert("Second Entry", std::bind(&BasicControlsNode::processFeedback, this, std::placeholders::_1));
-        interactive_markers::MenuHandler::EntryHandle sub_menu_handle = menu_handler_.insert("Submenu");
-        menu_handler_.insert(sub_menu_handle, "First Entry", std::bind(&BasicControlsNode::processFeedback, this, std::placeholders::_1));
-        menu_handler_.insert(sub_menu_handle, "Second Entry", std::bind(&BasicControlsNode::processFeedback, this, std::placeholders::_1));
+  // Defines menu entries and a submenu for interacting with the marker. Each menu item is associated with a callback function to process feedback when selected.
+  menu_handler_.insert("First Entry", std::bind(&BasicControlsNode::processFeedback, this, std::placeholders::_1));
+  menu_handler_.insert("Second Entry", std::bind(&BasicControlsNode::processFeedback, this, std::placeholders::_1));
 
-        frame_timer_ = create_wall_timer(
-            std::chrono::milliseconds(10), std::bind(&BasicControlsNode::frameCallback, this));
-    }
-    void BasicControlsNode::frameCallback()
-    {
-        static uint32_t counter = 0;
+  interactive_markers::MenuHandler::EntryHandle sub_menu_handle = menu_handler_.insert("Submenu");
+  menu_handler_.insert(sub_menu_handle, "First Entry", std::bind(&BasicControlsNode::processFeedback, this, std::placeholders::_1));
+  menu_handler_.insert(sub_menu_handle, "Second Entry", std::bind(&BasicControlsNode::processFeedback, this, std::placeholders::_1));
 
-        if (!tf_broadcaster_)
-        {
-            tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(shared_from_this());
-        }
+  frame_timer_ = create_wall_timer(
+    std::chrono::milliseconds(10), std::bind(&BasicControlsNode::frameCallback, this));
+}
 
-        // Get current time using rclcpp::Time
-        rclcpp::Time time_point = this->get_clock()->now();
+// Creates a simple cube marker
+visualization_msgs::msg::Marker BasicControlsNode::makeBox(const visualization_msgs::msg::InteractiveMarker & msg)
+{
+  visualization_msgs::msg::Marker marker;
+  marker.type = visualization_msgs::msg::Marker::CUBE;
+  marker.scale.x = msg.scale * 0.45;
+  marker.scale.y = msg.scale * 0.45;
+  marker.scale.z = msg.scale * 0.45;
+  marker.color.r = 0.5;
+  marker.color.g = 0.5;
+  marker.color.b = 0.5;
+  marker.color.a = 1.0;
 
-        // Create a TransformStamped object directly
-        geometry_msgs::msg::TransformStamped transform_msg; // In ROS 2, to use tf2::Stamped with transforms, you typically need to use the geometry_msgs::msg::TransformStamped directly
-        transform_msg.header.stamp = time_point;            // Use rclcpp::Time
-        transform_msg.header.frame_id = "base_link";
-        transform_msg.child_frame_id = "moving_frame";
-        transform_msg.transform.translation.x = 0.0;
-        transform_msg.transform.translation.y = 0.0;
-        transform_msg.transform.translation.z = sin(static_cast<double>(counter) / 140.0) * 2.0;
-        transform_msg.transform.rotation.x = 0.0;
-        transform_msg.transform.rotation.y = 0.0;
-        transform_msg.transform.rotation.z = 0.0;
-        transform_msg.transform.rotation.w = 1.0;
+  return marker;
+}
 
-        // Send the transform for moving frame
-        tf_broadcaster_->sendTransform(transform_msg);
+// Adds the cube marker as part of the interactive marker's control
+visualization_msgs::msg::InteractiveMarkerControl & BasicControlsNode::makeBoxControl(visualization_msgs::msg::InteractiveMarker & msg)
+{
+  visualization_msgs::msg::InteractiveMarkerControl control;
+  control.always_visible = true;
+  control.markers.push_back(makeBox(msg));
+  msg.controls.push_back(control);
 
-        // Create another transform for rotating frame
-        transform_msg.child_frame_id = "rotating_frame";
-        transform_msg.transform.translation.x = 0.0;
-        transform_msg.transform.translation.y = 0.0;
-        transform_msg.transform.translation.z = 0.0;
+  return msg.controls.back();
+}
 
-        // Use tf2 to create a Quaternion for rotation
-        tf2::Quaternion quat;
-        quat.setRPY(0.0, static_cast<double>(counter) / 140.0, 0.0);
+// This function is called periodically (every 10 ms), and it broadcasts two transforms: one for a moving frame and one for a rotating frame
+void BasicControlsNode::frameCallback()
+{
+  static uint32_t counter = 0;
 
-        // Convert tf2::Quaternion to geometry_msgs::msg::Quaternion
-        transform_msg.transform.rotation = tf2::toMsg(quat);
-        // Send the transform for rotating frame
-        tf_broadcaster_->sendTransform(transform_msg);
+  if (!tf_broadcaster_) {
+    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(shared_from_this());
+  }
 
-        counter++;
-    }
+  tf2::TimePoint tf_time_point(std::chrono::nanoseconds(this->get_clock()->now().nanoseconds()));
 
-    void BasicControlsNode::processFeedback(const visualization_msgs::msg::InteractiveMarkerFeedback::ConstSharedPtr &feedback)
-    {
-        std::ostringstream ss;
-        ss << "Feedback from marker '" << feedback->marker_name << "' "
-           << "at pose (x=" << feedback->pose.position.x
-           << ", y=" << feedback->pose.position.y
-           << ", z=" << feedback->pose.position.z << ")";
-        RCLCPP_INFO(this->get_logger(), "%s", ss.str().c_str());
-    }
+  tf2::Stamped<tf2::Transform> transform;
+  transform.stamp_ = tf_time_point;
+  transform.frame_id_ = "base_link";
+  transform.setOrigin(tf2::Vector3(0.0, 0.0, sin(static_cast<double>(counter) / 140.0) * 2.0));
+  transform.setRotation(tf2::Quaternion(0.0, 0.0, 0.0, 1.0));
 
-    visualization_msgs::msg::Marker BasicControlsNode::makeBox(const visualization_msgs::msg::InteractiveMarker &msg)
-    {
-        visualization_msgs::msg::Marker marker;
-        marker.type = visualization_msgs::msg::Marker::CUBE;
-        marker.scale.x = msg.scale * 0.45;
-        marker.scale.y = msg.scale * 0.45;
-        marker.scale.z = msg.scale * 0.45;
-        marker.color.r = 0.5;
-        marker.color.g = 0.5;
-        marker.color.b = 0.5;
-        marker.color.a = 1.0;
-        return marker;
-    }
+  geometry_msgs::msg::TransformStamped transform_msg;
+  transform_msg = tf2::toMsg(transform);
+  transform_msg.child_frame_id = "moving_frame";
+  tf_broadcaster_->sendTransform(transform_msg);
 
-    visualization_msgs::msg::InteractiveMarkerControl &BasicControlsNode::makeBoxControl(visualization_msgs::msg::InteractiveMarker &msg)
-    {
-        visualization_msgs::msg::InteractiveMarkerControl control;
-        control.always_visible = true;
-        control.markers.push_back(makeBox(msg));
-        msg.controls.push_back(control);
-        return msg.controls.back();
-    }
+  transform.setOrigin(tf2::Vector3(0.0, 0.0, 0.0));
+  tf2::Quaternion quat;
+  quat.setRPY(0.0, static_cast<double>(counter) / 140.0, 0.0);
+  transform.setRotation(quat);
+  transform_msg = tf2::toMsg(transform);
+  transform_msg.child_frame_id = "rotating_frame";
+  tf_broadcaster_->sendTransform(transform_msg);
 
-} // namespace interactive_marker_tutorials
+  counter++;
+}
+
+// Handles various types of feedback from the user, including button clicks and menu item selections
+void BasicControlsNode::processFeedback(const visualization_msgs::msg::InteractiveMarkerFeedback::ConstSharedPtr & feedback)
+{
+  std::ostringstream oss;
+  oss << "Feedback from marker '" << feedback->marker_name << "' / control '" << feedback->control_name << "'";
+
+  std::ostringstream mouse_point_ss;
+  if (feedback->mouse_point_valid) {
+    mouse_point_ss << " at " << feedback->mouse_point.x <<
+                   ", " << feedback->mouse_point.y <<
+                   ", " << feedback->mouse_point.z <<
+                   " in frame " << feedback->header.frame_id;
+  }
+
+  switch (feedback->event_type) {
+    case visualization_msgs::msg::InteractiveMarkerFeedback::BUTTON_CLICK:
+      oss << ": button click" << mouse_point_ss.str() << ".";
+      RCLCPP_INFO(get_logger(), "%s", oss.str().c_str());
+      break;
+
+    case visualization_msgs::msg::InteractiveMarkerFeedback::MENU_SELECT:
+      oss << ": menu item " << feedback->menu_entry_id << " clicked" << mouse_point_ss.str() << ".";
+      RCLCPP_INFO(get_logger(), "%s", oss.str().c_str());
+      break;
+
+    case visualization_msgs::msg::InteractiveMarkerFeedback::POSE_UPDATE:
+      oss << ": pose changed" <<
+          "\nposition = " <<
+          feedback->pose.position.x <<
+          ", " << feedback->pose.position.y <<
+          ", " << feedback->pose.position.z <<
+          "\norientation = " <<
+          feedback->pose.orientation.w <<
+          ", " << feedback->pose.orientation.x <<
+          ", " << feedback->pose.orientation.y <<
+          ", " << feedback->pose.orientation.z <<
+          "\nframe: " << feedback->header.frame_id <<
+          " time: " << feedback->header.stamp.sec << "sec, " <<
+          feedback->header.stamp.nanosec << " nsec";
+      RCLCPP_INFO(get_logger(), "%s", oss.str().c_str());
+      break;
+
+    case visualization_msgs::msg::InteractiveMarkerFeedback::MOUSE_DOWN:
+      oss << ": mouse down" << mouse_point_ss.str() << ".";
+      RCLCPP_INFO(get_logger(), "%s", oss.str().c_str());
+      break;
+
+    case visualization_msgs::msg::InteractiveMarkerFeedback::MOUSE_UP:
+      oss << ": mouse up" << mouse_point_ss.str() << ".";
+      RCLCPP_INFO(get_logger(), "%s", oss.str().c_str());
+      break;
+  }
+
+  server_->applyChanges();
+}
+
+// This function rounds the marker's position to the nearest 0.5 units (for grid alignment) when the pose is updated.
+void BasicControlsNode::alignMarker(const visualization_msgs::msg::InteractiveMarkerFeedback::ConstSharedPtr & feedback)
+{
+  geometry_msgs::msg::Pose pose = feedback->pose;
+
+  pose.position.x = round(pose.position.x - 0.5) + 0.5;
+  pose.position.y = round(pose.position.y - 0.5) + 0.5;
+
+  std::ostringstream oss;
+  oss << feedback->marker_name << ":" <<
+      " aligning position = " << pose.position.x << ", " <<
+      pose.position.y << ", " <<
+      pose.position.z;
+
+  RCLCPP_INFO(get_logger(), "%s", oss.str().c_str());
+  server_->setPose(feedback->marker_name, pose);
+}
+
+}  // namespace interactive_marker_tutorials
 
 #include <pluginlib/class_list_macros.hpp>
 PLUGINLIB_EXPORT_CLASS(interactive_marker_tutorials::BasicControlsNode, rclcpp::Node)
